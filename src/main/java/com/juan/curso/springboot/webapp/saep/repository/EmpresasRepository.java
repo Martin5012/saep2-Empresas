8555ee2a3a511054dedf164432c56ce04948a414
Santiagovs7 <villadasantasantiago208@gmail.com>
2025-06-16 18:45:44 -0500

    cambios

--- src/main/java/com/juan/curso/springboot/webapp/saep/repository/EmpresasRepository.java
+++ src/main/java/com/juan/curso/springboot/webapp/saep/repository/EmpresasRepository.java
@@ -2,9 +2,22 @@ package com.juan.curso.springboot.webapp.saep.repository;
 
 import com.juan.curso.springboot.webapp.saep.model.Empresas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface EmpresasRepository extends JpaRepository<Empresas, Long>
+import java.util.List;
 
-{
+public interface EmpresasRepository extends JpaRepository<Empresas, Long> {
 
+    @Query("SELECT e FROM Empresas e " +
+            "WHERE LOWER(e.nit) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.nombre) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.direccion) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.area) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.contacto) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.email) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.departamento) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.ciudad) LIKE LOWER(CONCAT('%', :criterio, '%')) " +
+            "OR LOWER(e.estado) LIKE LOWER(CONCAT('%', :criterio, '%'))")
+    List<Empresas> buscarPorCriterio(@Param("criterio") String criterio);
 }
