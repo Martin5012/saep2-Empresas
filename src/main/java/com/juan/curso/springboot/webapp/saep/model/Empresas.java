2121f14e3f7482ab0dfb1d65f5a5c22fe8b964c6
AndreZit0 <148222103+AndreZit0@users.noreply.github.com>
2025-06-17 18:19:12 -0500

    Revert "Cambios"

--- src/main/java/com/juan/curso/springboot/webapp/saep/model/Empresas.java
+++ src/main/java/com/juan/curso/springboot/webapp/saep/model/Empresas.java
@@ -8,14 +8,9 @@ public class Empresas
 {
     @Id
     @GeneratedValue(strategy = GenerationType. IDENTITY) // Auto-incremental
-<<<<<<< main
-    private Long id_empresas;
-    private  Integer id_usuarios;
-=======
     //Long para reconocerlo como llave primaria
     private Long id_empresas;
     private  Integer id_Usuarios;
->>>>>>> main
     private String nit;
     private String nombre;
     private String direccion;
@@ -34,12 +29,12 @@ public class Empresas
         this.id_empresas = id_empresas;
     }
 
-    public Integer getId_usuarios() {
-        return id_usuarios;
+    public Integer getId_Usuarios() {
+        return id_Usuarios;
     }
 
-    public void setId_usuarios(Integer id_usuarios) {
-        this.id_usuarios = id_usuarios;
+    public void setId_Usuarios(Integer id_Usuarios) {
+        this.id_Usuarios = id_Usuarios;
     }
 
     public String getNit() {
